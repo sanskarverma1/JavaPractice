18e38717bae471075817f97ef861c97bcc8a481f
Sanskar Verma <sanskarverma@Sanskars-MacBook-Air.local>
2025-02-23 21:02:30 +0530

    ArrayList and Comparator Examples

---
+++ src/ArrayList.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(80);
+        list.add(2,50);
+        list.set(3,50);
+
+// 		list.remove(1);
+
+        for(int x : list){
+            System.out.println(x);
+        }
+// 		System.out.println(list.get(0));
+// 		System.out.println(list.contains(5));
+
+
+        //addition to this list isnt allowed only the elements can be replaced
+        List<Integer> aslist = Arrays.asList(1, 2, 3, 4);
+        //aslist.add(1); //add not allowed
+        aslist.set(1,999); //replace 1st index from 2 to 999
+        //aslist.remove(Integer.valueOf(3)); //remove not allowed
+        System.out.println(aslist);
+
+        //wrap with new ArrayList<> then everything will work with Arrays.asList
+        List<Integer> modifyableList = new ArrayList<> (Arrays.asList(6, 7, 8, 9));
+        modifyableList.add(2);
+        modifyableList.remove(Integer.valueOf(7));
+        System.out.println(modifyableList);
+
+
+        //unmodifiable list neither add nor set allowed
+        List<Integer> integers = List.of(1, 2, 3);
+        System.out.println(integers);
+
+//        Summary:
+//
+//        Arrays.asList(): Fixed size, can modify elements
+//        List.of(): Completely immutable
+//        new ArrayList<>(Arrays.asList(...)): Fully modifiable
+//        Collections.unmodifiableList(): Makes any list immutable
+
+    }
+}
+
---
+++ src/Comparatorexample.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+
+class Student{
+    private  String name;
+    private double gpa;
+
+    public Student(String name, double gpa) {
+        this.gpa = gpa;
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
+public class Comparatorexample {
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Dlice",3.5));
+        students.add(new Student("Bob",3.7));
+        students.add(new Student("Charlie",3.5));
+        students.add(new Student("Akshit",3.8));
+
+        // bare metal way of writing own comparator using lambda expression
+        students.sort((a,b)->{
+            /*
+            doing this because gpa is in double and if we directly do (int) (a.getGpa()-b.getGpa())
+             it will not correctly subtract and after typecasting it to int it will give same result and ignore decimals so it wont be sorted correctly
+            */
+            if(a.getGpa()-b.getGpa()>0) return 1;
+            else if(a.getGpa()-b.getGpa()<0) return -1;
+            else return 0;
+
+        });
+        System.out.println(students);
+
+        /*
+        after java8 method referencing(Student::getGpa) was introduced so we dont have to write our own
+        comparator from scratch
+        */
+        Comparator<Student> comparator = Comparator.comparing(Student::getGpa).reversed().thenComparing(Student::getName);
+        students.sort(comparator);
+        System.out.println(students);
+
+        //using collections
+        Collections.sort(students,comparator);
+        System.out.println(students);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(3);
+
+        list.sort((a,b)->b-a); // asc = ((a,b)->a-b), desc = ((a,b)->b-a)
+
+        System.out.println(list);
+
+        List<String> words = new ArrayList<> (List.of("shiva","sakshi","don","Aamenaa"));
+        words.sort((a,b)->a.length()-b.length());
+        System.out.println(words);
+    }
+}
